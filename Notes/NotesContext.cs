fb32025c29b892eb9c59dc64c6c879628f1f2511
Александр Pistak <pistak@deeplay.io>
2024-03-27 23:26:18 +0700

    Добавьте файлы проекта.

---
+++ Notes/NotesContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Notes
+{
+    class NotesContext : DbContext
+    {
+        public NotesContext() : base("DbConnection")
+        {
+            Console.WriteLine("Done");
+        }
+        public DbSet<Notes> Notes { get; set; } //создали таблицу
+        public void Show()
+        {
+            foreach (var note in Notes)
+            {
+                Console.WriteLine($"{note.Id} {note.Name} {note.Text} {note.Date}");
+            }
+        }
+        public void Insert(string name, string text, DateTime date)
+        {
+            Notes.Add(new Notes 
+            {
+                Name = name,
+                Text = text,
+                Date = date
+            });
+            SaveChanges();
+        }
+    }
+}
---
+++ Notes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NotesContext notes = new NotesContext())
+            {
+                notes.Insert("Note1", "MyFirstNote", DateTime.Now);
+                notes.Show();
+            }
+        }
+    }
+}
